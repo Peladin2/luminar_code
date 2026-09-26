b16b17368d00a597bf2c6e3ce48e01ba19792074
rick <vizz669955@gmail.com>
2026-09-26 16:42:25 -0300

    feat: agrega modelo Noticia con CRUD

---
+++ backend/models/Noticia.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once __DIR__ . '/../config/Database.php';
+
+class Noticia
+{
+    private $conexion;
+
+    public function __construct()
+    {
+        $db = new Database();
+        $this->conexion = $db->conexion;
+    }
+
+    public function obtenerTodos()
+    {
+        $sql = 'SELECT ID_Noticia, Titulo, Fecha, Descripcion, URL_Imagen, ID_Administrador FROM Noticia ORDER BY Fecha DESC';
+        $consulta = $this->conexion->query($sql);
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function obtenerPorId($id)
+    {
+         $sql = 'SELECT ID_Noticia, Titulo, Fecha, Descripcion, URL_Imagen, ID_Administrador FROM Noticia WHERE ID_Noticia = :id';
+         $consulta = $this->conexion->prepare($sql);
+         $consulta->bindParam(':id', $id, PDO::PARAM_INT);
+         $consulta->execute();
+
+         $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+         if ($resultado) {
+            return $resultado;
+         } else {
+            return null;
+         }
+    }
+
+    public function crear($titulo, $fecha, $descripcion, $urlImagen, $idAdministrador)
+    {
+        $sql = 'INSERT INTO Noticia (Titulo, Fecha, Descripcion, URL_Imagen, ID_Administrador) VALUES (:titulo, :fecha, :descripcion, :urlImagen, :idAdministrador)';
+        $consulta = $this->conexion->prepare($sql);
+        $consulta->bindParam(':titulo', $titulo);
+        $consulta->bindParam(':fecha', $fecha);
+        $consulta->bindParam(':descripcion', $descripcion);
+        $consulta->bindParam(':urlImagen', $urlImagen);
+        $consulta->bindParam(':idAdministrador', $idAdministrador, PDO::PARAM_INT);
+        $consulta->execute();
+
+        return $this->conexion->lastInsertId();
+    }
+
+    public function actualizar ($id, $titulo, $fecha, $descripcion, $urlImagen)
+    {
+        $sql = 'UPDATE Noticia SET Titulo = :titulo, Fecha = :fecha, Descripcion = :descripcion, URL_Imagen = :urlImagen WHERE ID_Noticia = :id';
+        $consulta = $this->conexion->prepare($sql);
+        $consulta->bindParam(':titulo', $titulo);
+        $consulta->bindParam(':fecha', $fecha);
+        $consulta->bindParam(':descripcion', $descripcion);
+        $consulta->bindParam(':urlImagen', $urlImagen);
+        $consulta->bindParam(':id', $id, PDO::PARAM_INT);
+        $consulta->execute();
+
+        if ($consulta->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function eliminar ($id)
+    {
+        $sql = 'DELETE FROM Noticia WHERE ID_Noticia = :id';
+        $consulta = $this->conexion->prepare($sql);
+        $consulta->bindParam(':id', $id, PDO::PARAM_INT);
+        $consulta->execute();
+
+        if ($consulta->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
